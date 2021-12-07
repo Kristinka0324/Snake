b27afcbaa0138cd637a045116c605127726dca82
Kristina <kristinka.antipova94@gmail.com>
2021-12-07 21:00:47 +0500

    Создали произвольную горизонтальную линию

---
+++ Snake/HorizontalLine.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake
+{
+    class HorizontalLine    //список точек
+    {
+        List<Point> pList;    //горизонтальная линия это тот же список точек
+
+
+        // При создании гориз линии передадим сюда параметры,
+        // которые будут задавать длинну и положение линии.
+        // зададим координату по оси Х- левой точки гор линии, координату по оси Х- правого конца, координату по оси У и символ.
+        public HorizontalLine(int xLeft, int xRight, int y, char sym)                                                     
+        {
+            pList = new List<Point>();      // создали пустой список
+            for(int x = xLeft; x <= xRight; x++)
+            // в данном цикле на каждой итерации переменна Х будет принимать значения начиная от xLeft, заканчивая xRignt.
+            {
+                Point p = new Point(x, y, sym);  
+                pList.Add(p);
+            }
+           
+        }
+
+        //теперь выведем эти точки на экран
+        
+        public void Draw() // для этого используем аналогичный метод Draw для вывода на экран, из класса Point 
+        {
+            foreach(Point p in pList) //пробежимся по всем точкам
+            {
+                p.Draw(); // и выведем их на экран
+            }
+        }
+    }
+}
--- Snake/Program.cs
+++ Snake/Program.cs
@@ -8,13 +8,13 @@ namespace Snake
         static void Main(string[] args)
         {
             
-            Point p1 = new Point(1,3, '*');  //  конструктор который принимает координаты точки. объект класса Point.
-            p1.Draw();  // и выводит на экран
+            Point p1 = new Point(1,3, '*'); 
+            p1.Draw();  
 
             Point p2 = new Point(4, 5, '#');
             p2.Draw();
 
-            HorizontalLine Line = new HorizontalLine(); //вызвали конструктор из класса HorizontalLine (объект класса HorizontalLine)
+            HorizontalLine Line = new HorizontalLine(5, 10, 8 , '+');
             Line.Draw();
 
             Console.ReadLine();
