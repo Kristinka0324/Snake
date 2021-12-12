93dc9a197e332f7603beea066942b5dbfd08d158
Kristina <kristinka.antipova94@gmail.com>
2021-12-12 15:25:46 +0500

    Обрабатываем столкновения

--- Snake/Point.cs
+++ Snake/Point.cs
@@ -6,78 +6,70 @@ using System.Threading.Tasks;
 
 namespace Snake
 {
+	class Point
+	{
+		public int x;
+		public int y;
+		public char sym;
 
-    // Создали новый класс. Point
-    // Теперь наш класс состояит не тольуо из данных, но из из функции
-    class Point
-    {
-        public int x;       //данные класса Point
-        public int y;        //данные класса Point
-        public char sym;      //данные класса Point
+		public Point()
+		{
+		}
 
-        public Point()
-        {
-        }
+		public Point(int x, int y, char sym)
+		{
+			this.x = x;
+			this.y = y;
+			this.sym = sym;
+		}
 
-        public Point(int _x, int _y, char _sym)   // конструктор  
-        {
-            x = _x;
-            y = _y;
-            sym = _sym;
-        }
+		public Point(Point p)
+		{
+			x = p.x;
+			y = p.y;
+			sym = p.sym;
+		}
 
-        public Point(Point p)
-        {
-            x = p.x;
-            y = p.y;
-            sym = p.sym;
-        }
+		public void Move(int offset, Direction direction)
+		{
+			if (direction == Direction.RIGHT)
+			{
+				x = x + offset;
+			}
+			else if (direction == Direction.LEFT)
+			{
+				x = x - offset;
+			}
+			else if (direction == Direction.UP)
+			{
+				y = y - offset;
+			}
+			else if (direction == Direction.DOWN)
+			{
+				y = y + offset;
+			}
+		}
 
-        public void Move(int offset, Direction direction)
-        {
-            if (direction == Direction.RIGHT)
-            {
-                x = x + offset;
-            }
-            if (direction == Direction.LEFT)
-            {
-                x = x - offset;
-            }
-            if (direction == Direction.UP)
-            {
-                y = y - offset;
-            }
-            if (direction == Direction.DOWN)
-            {
-                y = y + offset;
-            }
-        }
+		public bool IsHit(Point p)
+		{
+			return p.x == this.x && p.y == this.y;
+		}
 
-        public bool IsHit(Point p)
-        {
-            return p.x == this.x && p.y == this.y;
-        }
+		public void Draw()
+		{
+			Console.SetCursorPosition(x, y);
+			Console.Write(sym);
+		}
 
+		public void Clear()
+		{
+			sym = ' ';
+			Draw();
+		}
 
-
-        public void Draw()     // функция которая будет выводить наши символы.
-        {
-            Console.SetCursorPosition(x, y);
-            Console.Write(sym);
-        }
-
-        public void Clear()
-        {
-            sym = ' ';
-            Draw();
-        }
-
-
-        public override string ToString()
-        {
-            return x + ", " + y + ", " + sym;
-        }
-
-    
-    }
+		public override string ToString()
+		{
+			return x + ", " + y + ", " + sym;
+		}
+	}
 }
--- Snake/Program.cs
+++ Snake/Program.cs
@@ -8,32 +8,29 @@ namespace Snake
 {
 	class Program
 	{
-		static void Main( string[] args )
+		static void Main(string[] args)
 		{
-			// Console.SetBufferSize( 80, 25 );
+			// Console.SetBufferSize(80, 25);
 
-			// Отрисовка рамочки
-			HorizontalLine upLine = new HorizontalLine( 0, 78, 0, '+' );
-			HorizontalLine downLine = new HorizontalLine( 0, 78, 24, '+' );
-			VerticalLine leftLine = new VerticalLine( 0, 24, 0, '+' );
-			VerticalLine rightLine = new VerticalLine( 0, 24, 78, '+' );
-			upLine.Draw();
-			downLine.Draw();
-			leftLine.Draw();
-			rightLine.Draw();
-			
+			Walls walls = new Walls(80, 25);
+			walls.Draw();
+
 			// Отрисовка точек			
-			Point p = new Point( 4, 5, '*' );
-			Snake snake = new Snake( p, 4, Direction.RIGHT );
+			Point p = new Point(4, 5, '*');
+			Snake snake = new Snake(p, 4, Direction.RIGHT);
 			snake.Draw();
 
-			FoodCreator foodCreator = new FoodCreator( 80, 25, '$' ); // создали новый класс FoodCreator и через конструктор создаем символ еды змейки $ 
+			FoodCreator foodCreator = new FoodCreator(80, 25, '$');
 			Point food = foodCreator.CreateFood();
 			food.Draw();
 
 			while (true)
 			{
-				if(snake.Eat( food ) )
+				if (walls.IsHit(snake) || snake.IsHitTail())
+				{
+					break;
+				}
+				if (snake.Eat(food))
 				{
 					food = foodCreator.CreateFood();
 					food.Draw();
@@ -41,16 +38,16 @@ namespace Snake
 				else
 				{
 					snake.Move();
-				}					
+				}
 
-				Thread.Sleep( 400 );
-
+				Thread.Sleep(100);
 				if (Console.KeyAvailable)
 				{
 					ConsoleKeyInfo key = Console.ReadKey();
-					snake.HandleKey( key.Key );
+					snake.HandleKey(key.Key);
 				}
 			}
 		}
+
 	}
 }
